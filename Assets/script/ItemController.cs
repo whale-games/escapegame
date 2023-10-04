cbb1b979188f650d9cbf95b7af6c401fc05f96a4
MAIOTAchannel <h20051007@gmail.com>
2023-10-04 22:13:24 +0900

    アイテムの取得と使用

---
+++ Assets/script/ItemController.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> panels;
+    private ItemUtils itemUtils;
+
+    private void Start() {
+        itemUtils = GetComponent<ItemUtils>(); //アイテムUtilsの取得
+    }
+
+    //画面クリック時の処理
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+
+                //アイテムをクリックした時の処理
+                if(hit.collider.tag == "Item"){
+                    int returncode = itemUtils.AddItem(hit.collider.gameObject, panels);
+                    //エラー処理
+                    //-1 配列がいっぱい
+                    switch(returncode){
+                        case -1:
+                            Debug.Log("配列がいっぱいです。");
+                            break;
+                    }
+                }
+
+                //アイテムを使用する場所をクリックした時の処理
+                if(hit.collider.tag == "ItemUse" && itemUtils.choosingGameObject != null){
+                    itemUtils.RemoveItem(itemUtils.choosingGameObject.transform.GetChild(0).gameObject);
+                    Debug.Log("アイテムを使用した！");
+                }else if (itemUtils.choosingGameObject == null){
+                    Debug.Log("アイテムを指定していません");
+                }
+            }
+
+            //アイテムを選択時しているときにパネルの色を戻す
+            if(itemUtils.choosingGameObject != null) {
+                Image image = itemUtils.choosingGameObject.GetComponent<Image>();
+                image.color = itemUtils.HexToRGB("#808080");
+                itemUtils.choosingGameObject = null;
+            }
+        }
+    }
+
+    //アイテム欄をクリックしたときの処理　ﾖｼｯ
+    public void buttonClick(GameObject gameObject)
+    {
+        //パネルがリストに入っているか確認
+        if(!panels.Contains(gameObject))
+        {
+            Debug.Log("そのパネルはリストに入っていません");
+            return;
+        }
+
+        //選択したパネルにアイテムが入っているか確認
+        if(gameObject.transform.childCount == 0){
+            Debug.Log("そのパネルにはアイテムが入っていません");
+            return;
+        }
+
+
+        Image image;
+
+        //選択されていたパネルの色を薄くする
+        if(itemUtils.choosingGameObject != null) {
+            image = itemUtils.choosingGameObject.GetComponent<Image>();
+            image.color = itemUtils.HexToRGB("#808080");
+        }
+
+        //選択したパネルの色を濃くする
+        image = gameObject.GetComponent<Image>();
+        image.color = itemUtils.HexToRGB("#FFFFFF");
+        itemUtils.choosingGameObject = gameObject;
+    }
+}
---
+++ Assets/script/ItemUtils.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUtils : MonoBehaviour{
+    public List<string> itemList = new List<string>(5);
+    public GameObject choosingGameObject = null;
+
+        //アイテムを持ち物に追加
+    public int AddItem(GameObject item, List<GameObject> itemPanelList)
+    {
+        //要素数が最大の場合に戻す
+        if(itemList.Count == 5){
+            return -1;
+        }
+        itemList.Add(item.name);
+        item.transform.parent = itemPanelList[itemList.IndexOf(item.name)].transform;
+        item.transform.localPosition = new Vector3(0,0,-11);
+        item.transform.localScale = new Vector3(500,500,500);
+        item.layer = 6;
+        return 0;
+    }
+
+    //アイテムを削除
+    public int RemoveItem(GameObject item)
+    {
+        itemList.Remove(item.name);
+        Destroy(item);
+        return 0;
+    }
+
+    //カラーコードからColor型に変換
+    public Color HexToRGB(string hex)
+    {
+        if(ColorUtility.TryParseHtmlString(hex,out Color color)) return color;
+        else return Color.black;
+    }
+
+}
